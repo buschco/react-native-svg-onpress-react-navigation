2666453476f7b3f7ebcf8c87a13f86a03700c014
buschco <colin@busch.dev>
2025-07-30 10:45:03 +0200

    onpress svg

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -4,18 +4,46 @@
  *
  * @format
  */
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ScrollView, StyleSheet } from 'react-native';
+import Svg, { Rect } from 'react-native-svg';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+const colors = ['blue', 'red'];
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+function HomeScreen() {
+  const [pressed, setPressed] = React.useState<number>(0);
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Svg viewBox={`0 0 ${200} ${200}`}>
+        <Rect
+          onPress={() => setPressed(p => 1 + p)}
+          x={0}
+          y={0}
+          height={200}
+          width={200}
+          fill={colors[pressed % 2]}
+        />
+      </Svg>
+    </ScrollView>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <>
+      <HomeScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
